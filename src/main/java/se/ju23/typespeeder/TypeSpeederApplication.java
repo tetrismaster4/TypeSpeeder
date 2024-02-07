22649a86fd6228ed918496061e884d9b45d45169
Oskar Ray-Frayssinet <oskar.rf@live.se>
2024-02-07 13:56:48 +0100

    Adding project

---
+++ src/main/java/se/ju23/typespeeder/TypeSpeederApplication.java
@@ -0,0 +1,13 @@
+package se.ju23.typespeeder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TypeSpeederApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TypeSpeederApplication.class, args);
+    }
+
+}
---
+++ src/test/java/se/ju23/typespeeder/TypeSpeederApplicationTests.java
@@ -0,0 +1,13 @@
+package se.ju23.typespeeder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TypeSpeederApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
